3d625fcf2dd02e7c70cbaee1c3d90efe6e66f489
Shubham Gupta <shugupta5@publicisgroupe.net>
2023-03-25 20:13:10 +0530

    eureka

---
+++ src/test/java/com/eureka/discovery/app/MicroservicesDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.discovery.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
